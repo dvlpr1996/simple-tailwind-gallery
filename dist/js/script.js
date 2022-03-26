e92ccb2bb35833b6e365b0a5deb2ca5696965dc8
dvlpr1996 <nimajahanbakhshian@gmail.com>
2022-03-26 21:16:27 +0430

    add lightbox2 and implement gallery with lightbox2

---
+++ dist/js/script.js
@@ -0,0 +1,7 @@
+document.getElementById("year").innerHTML = new Date().getFullYear();
+
+lightbox.option({
+	'resizeDuration': 100,
+	'wrapAround': true,
+	'alwaysShowNavOnTouchDevices' : true,
+});
