6d4e2809e11ec6a2bbc027865bd08c3585323b44
dvlpr1996 <nimajahanbakhshian@gmail.com>
2022-03-28 20:05:37 +0430

    implement the image filtering feature based on their subject

--- dist/js/script.js
+++ dist/js/script.js
@@ -3,5 +3,17 @@ document.getElementById("year").innerHTML = new Date().getFullYear();
 lightbox.option({
 	'resizeDuration': 100,
 	'wrapAround': true,
-	'alwaysShowNavOnTouchDevices' : true,
+	'alwaysShowNavOnTouchDevices': true,
+});
+
+const $mediaElements = $('.media');
+
+$('.filter_link').click(function (e) {
+	e.preventDefault();
+	const filterVal = $(this).data('filter');
+	if (filterVal === 'all') {
+		$mediaElements.show();
+	} else {
+		$mediaElements.hide().filter('.' + filterVal).show();
+	}
 });
